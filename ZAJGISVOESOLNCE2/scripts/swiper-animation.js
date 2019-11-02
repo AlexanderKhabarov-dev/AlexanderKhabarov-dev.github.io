622bfdfaf9ec12dc9b093c86e55692a3f8beb508
AlexanderKhabarovv <52774642+AlexanderKhabarovv@users.noreply.github.com>
2019-11-02 14:30:12 +0400

    slider-adaptive

--- ZAJGISVOESOLNCE2/scripts/swiper-animation.js
+++ ZAJGISVOESOLNCE2/scripts/swiper-animation.js
@@ -23,3 +23,51 @@ let swiper = new Swiper('.swiper-container', {
       clickable: true,
     },
   });
+
+if (document.body.clientWidth<540){
+  let advantagesSwiper = new Swiper('.swiper-container_2',{
+    slidesPerView: 1,
+    loop: true,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
+
+  let gettingSwiper = new Swiper('.swiper-container3',{
+    slidesPerView: 1,
+    loop: false,
+    spaceBetween: 20,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
+}
+
+else if(document.body.clientWidth>540){
+  let element = document.querySelectorAll('.swiper-slide');
+  let element2 = document.querySelectorAll('.swiper-button-prev');
+  let element3 = document.querySelectorAll('.swiper-button-next');
+  let element4 = document.querySelectorAll('.swiper-container_2');
+  let element5 = document.querySelectorAll('.swiper-container3');
+  let element6 = document.querySelectorAll('.swiper-wrapper');
+  element.forEach(function(item) {
+    item.classList.remove("swiper-slide");
+  })
+  element2.forEach(function(item) {
+    item.classList.remove("swiper-button-prev");
+  })
+  element3.forEach(function(item) {
+    item.classList.remove("swiper-button-next");
+  })
+  element4.forEach(function(item) {
+    item.classList.remove("swiper-container_2");
+  })
+  element5.forEach(function(item) {
+    item.classList.remove("swiper-container3");
+  })
+  element5.forEach(function(item) {
+    item.classList.remove("swiper-wrapper");
+  })
+  
+}
