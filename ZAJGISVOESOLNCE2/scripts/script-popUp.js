9f3f645b0b82c5fa3f0707ca6c910521377a614b
AlexanderKhabarovv <52774642+AlexanderKhabarovv@users.noreply.github.com>
2019-11-04 01:07:34 +0400

    commit popUps

--- ZAJGISVOESOLNCE2/scripts/script-popUp.js
+++ ZAJGISVOESOLNCE2/scripts/script-popUp.js
@@ -2,7 +2,9 @@
 // ==== POP-UP ===
 function show(state)
 {
-document.getElementById('popUp').style.display = state;		
+document.getElementById('popUpForm').style.display = state;		
+document.getElementById('popUpSuccessfulPayment').style.display = state;
+document.getElementById('popUpFailedPayment').style.display = state;		
 }	
 
 // === /POP-up ===
