6d567f7c4fa8236efd807fd143e6b8a20d7bc235
AlexanderKhabarovv <52774642+AlexanderKhabarovv@users.noreply.github.com>
2019-11-03 17:11:34 +0400

    test-others

--- ZAJGISVOESOLNCE2/scripts/swiper-animation.js
+++ ZAJGISVOESOLNCE2/scripts/swiper-animation.js
@@ -1,12 +1,14 @@
-let swiper = new Swiper('.swiper-container', {
+let othersSwiper = new Swiper('.swiper-container', {
     effect: 'coverflow',
     slidesPerView: 3,
     initialSlide: 1,
     loop: true,
+    autoplay: {
+      delay: 10000,
+    },
     fadeEffect: {
         crossFade: true
       },
-      
     coverflowEffect: {
       rotate: 0,
       stretch: -75,
@@ -22,6 +24,7 @@ let swiper = new Swiper('.swiper-container', {
       el: '.swiper-pagination',
       clickable: true,
     },
+
   });
 
 if (document.body.clientWidth<540){
@@ -54,29 +57,28 @@ if (document.body.clientWidth<540){
 }
 
 else if(document.body.clientWidth>540){
-  let element = document.querySelectorAll('.swiper-slide');
-  let element2 = document.querySelectorAll('.swiper-button-prev');
-  let element3 = document.querySelectorAll('.swiper-button-next');
-  let element4 = document.querySelectorAll('.swiper-container_2');
-  let element5 = document.querySelectorAll('.swiper-container3');
-  let element6 = document.querySelectorAll('.swiper-wrapper');
-  element.forEach(function(item) {
+  let slide = document.querySelectorAll('.swiper-slide');
+  let container2 = document.querySelectorAll('.swiper-container_2');
+  let container3 = document.querySelectorAll('.swiper-container3');
+  let wrapper = document.querySelectorAll('.swiper-wrapper');
+  let othersSlide = document.querySelectorAll(".section-others__content");
+  slide.forEach(function(item) {
     item.classList.remove("swiper-slide");
   })
-  element2.forEach(function(item) {
-    item.classList.remove("swiper-button-prev");
-  })
-  element3.forEach(function(item) {
-    item.classList.remove("swiper-button-next");
-  })
-  element4.forEach(function(item) {
+  container2.forEach(function(item) {
     item.classList.remove("swiper-container_2");
   })
-  element5.forEach(function(item) {
+  container3.forEach(function(item) {
     item.classList.remove("swiper-container3");
   })
-  element6.forEach(function(item) {
+  wrapper.forEach(function(item) {
     item.classList.remove("swiper-wrapper");
-  })
-  
+  })  
+  othersSlide.forEach(function(item) {
+    item.classList.add("swiper-slide");
+  })  
 }
+let othersWrapper = document.querySelector(".section-others__wrapper");
+othersWrapper.classList.add("swiper-wrapper");
+
+
