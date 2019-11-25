5df7260f029c04086d840e2b6937e9914e021a17
AlexanderKhabarovv <52774642+AlexanderKhabarovv@users.noreply.github.com>
2019-11-25 12:33:20 +0400

    fix

---
+++ blackthorn/scripts/scroll-init.js
@@ -0,0 +1,7 @@
+new fullpage('#fullpage', {
+    //options here
+    anchors: ['firstPage', 'secondPage', 'thirdPage'],
+    autoScrolling:true,
+    scrollHorizontally: true,
+});
+fullpage_api.setAllowScrolling(true);
