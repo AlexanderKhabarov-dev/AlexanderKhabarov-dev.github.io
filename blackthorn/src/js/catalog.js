1c2f7678c06d3636005a0fd8a4b708ae88defca7
AlexanderKhabarov-dev <sanek737373@inbox.ru>
2019-12-25 23:36:21 +0500

    123

--- blackthorn/src/js/catalog.js
+++ blackthorn/src/js/catalog.js
@@ -1,10 +1,10 @@
 let productsCatalog = [
-    {id:'elem1' , name:'Белое хлопковое платье с кружевом' ,            price:1700 ,  img:'../../src/img/products__card-image.jpg', link: '#'}, //Данные, которые по смыслу - будут заполняться позже
-    {id:'elem2' , name:'Венок из кукольных тел в розовой проволоке' ,   price:1000 ,  img:'../../src/img/card3.jpg', url: '#'},
-    {id:'elem3' , name:'Винтажная брошь причудливой формы' ,            price:5000 ,  img:'../../src/img/card2.jpg', url: '#'},
-    {id:'elem4' , name:'Косуха из натуральной кожи с бахромой.' ,       price:3200 ,  img:'../../src/img/card3.jpg', url: '#'},
-    {id:'elem5' , name:'Удлиненная юбка, полностью из хлопка' ,         price:4700 ,  img:'../../src/img/card6.jpg', url: '#'},
-    {id:'elem6' , name:'Сумка из натуральной кожи с замком' ,           price:3100 ,  img:'../../src/img/card5.jpg', url: '#'},
-    {id:'elem7' , name:'Ботинки DR MARTENS' ,                           price:800 ,   img:'../../src/img/card4.jpg', url: '#'},
-    {id:'elem8' , name:'Белое хлопковое платье с кружевом' ,            price:1700 ,  img:'../../src/img/card5.jpg', url: '#'}
+    {id:'elem1' , name:'Белое хлопковое платье с кружевом' ,            price:1700 ,  img:'src/img/products__card-image.jpg', link: '#'}, //Данные, которые по смыслу - будут заполняться позже
+    {id:'elem2' , name:'Венок из кукольных тел в розовой проволоке' ,   price:1000 ,  img:'src/img/card3.jpg', url: '#'},
+    {id:'elem3' , name:'Винтажная брошь причудливой формы' ,            price:5000 ,  img:'src/img/card2.jpg', url: '#'},
+    {id:'elem4' , name:'Косуха из натуральной кожи с бахромой.' ,       price:3200 ,  img:'src/img/card3.jpg', url: '#'},
+    {id:'elem5' , name:'Удлиненная юбка, полностью из хлопка' ,         price:4700 ,  img:'src/img/card6.jpg', url: '#'},
+    {id:'elem6' , name:'Сумка из натуральной кожи с замком' ,           price:3100 ,  img:'src/img/card5.jpg', url: '#'},
+    {id:'elem7' , name:'Ботинки DR MARTENS' ,                           price:800 ,   img:'src/img/card4.jpg', url: '#'},
+    {id:'elem8' , name:'Белое хлопковое платье с кружевом' ,            price:1700 ,  img:'src/img/card5.jpg', url: '#'}
 ]
