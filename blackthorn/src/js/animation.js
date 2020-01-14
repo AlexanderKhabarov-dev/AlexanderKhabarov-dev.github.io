2c716da8ee3ff723d526f84225e76b3edadcf1e3
AlexanderKhabarov-dev <sanek737373@inbox.ru>
2020-01-14 20:16:53 +0500

    123

--- blackthorn/src/js/animation.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    let preloaderText = document.getElementById('preloaderText');
-    let progressbar = document.getElementById('progressbar');
-
-    TweenLite.to(preloaderText, 3, {x:"860px"} )
-    TweenLite.to(preloader,     3, {x:"855px"} )
-  }
