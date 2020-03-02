dbf507139da7cc457673efec9b60c45c3642f6ba
AlexanderKhabarov-dev <sanek737373@inbox.ru>
2020-03-02 12:59:57 +0300

    123

---
+++ testJS/js/script.js
@@ -0,0 +1,7 @@
+let inputIn = document.querySelector('.input-in');
+let btn = document.querySelector('.btn');
+let text = document.getElementById('text');
+
+btn.onclick = () =>{
+    text.style.display = "block"
+}
