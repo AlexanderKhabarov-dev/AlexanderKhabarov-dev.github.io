3857ea7077caee40f88aafa8e7b22d48648ad756
AlexanderKhabarov-dev <sanek737373@inbox.ru>
2020-04-23 16:28:41 +0400

    fix -animation

--- headphones/build/js/preloader.js
+++ headphones/build/js/preloader.js
@@ -1,16 +1,11 @@
 window.onload = () =>{
     let preloader = document.querySelector('.preloader');
-    // let item1 = document.querySelector('.preloader__item_1');
-    // let item2 = document.querySelector('.preloader__item_2');
 
     TweenMax.to(preloader, 1, {
         display: 'none', 
         opacity: 0,
-        delay: 2,
+        delay: .5,
     })
 
-    // TweenMax.to([item1, item2,], 1 , {opacity: 1});
-    // TweenMax.staggerFrom([item1, item2,], 1.5, {rotation: 360,repeat: -1,},0.25);
-
-    // TweenMax.fromTo(item1,  1,  { x: 50}, { x: 0});
+   
 }
