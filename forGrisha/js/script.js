0a64da46761a6df295e46386da6ae56adda015e4
AlexanderKhabarov-dev <sanek737373@inbox.ru>
2020-04-26 14:01:08 +0400

    test fix

--- forGrisha/js/script.js
+++ forGrisha/js/script.js
@@ -1,12 +1,12 @@
 let content = document.querySelector('.content');
 
-window.addEventListener('orientationchange', rotate)
+window.addEventListener('mousemove', rotate)
 
 function rotate(event){
     const image = this.querySelector('.content-img');
     const halfHeight = image.offsetHeight / 2;
     const halfWidth = image.offsetWidth / 2;
-    //image.style.transform =`rotateX(${+ -(event.offsetY - halfHeight)/10}deg)  rotateY(${(event.offsetX - halfWidth)/10}deg)`
-    image.style.transform = `rotateX${event.orentation}deg rotateY${event.orentation}deg`
-    console.log(event.orentation)
+    image.style.transform =`rotateX(${+ -(event.offsetY - halfHeight)/10}deg)  rotateY(${(event.offsetX - halfWidth)/10}deg)`
+    // image.style.transform = `rotateX${event.orentation}deg rotateY${event.orentation}deg`
+    // console.log(event.orentation)
 }
