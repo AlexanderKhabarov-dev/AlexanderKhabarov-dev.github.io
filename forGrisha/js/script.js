39b964de379fda78e1647575b149254c556d5bab
AlexanderKhabarov-dev <sanek737373@inbox.ru>
2020-04-26 14:04:08 +0400

    fix 4

--- forGrisha/js/script.js
+++ forGrisha/js/script.js
@@ -3,7 +3,7 @@ let content = document.querySelector('.content');
 window.addEventListener('mousemove', rotate)
 
 function rotate(event){
-    const image = this.querySelector('.content-img');
+    const image = document.querySelector('.content-img');
     const halfHeight = image.offsetHeight / 2;
     const halfWidth = image.offsetWidth / 2;
     image.style.transform =`rotateX(${+ -(event.offsetY - halfHeight)/10}deg)  rotateY(${(event.offsetX - halfWidth)/10}deg)`
