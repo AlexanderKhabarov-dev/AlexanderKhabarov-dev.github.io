4701f56584cfdb388268a94064cb9df80539e989
AlexanderKhabarov-dev <sanek737373@inbox.ru>
2020-04-27 14:37:38 +0400

    fix

--- test_2/js/parallax.js
+++ test_2/js/parallax.js
@@ -5,13 +5,15 @@ let bolt = document.querySelector('.main__decoration-bolt');
 
 function onMotionChange(e) {
 
-    let agZ = e.accelerationIncludingGravity.z + 1;
+    let agZ = e.accelerationIncludingGravity.z -5;
     let agX = e.accelerationIncludingGravity.x;
 
     bolt.style.transform = `translateX(${agX*13}px)
                             translateY(${agZ*7}px) 
                             rotateX(${agZ*5}deg)
                             rotateY(${agX*13}deg)`
+
+    circle.agZ = -10;
 
     circle.style.transform = `translateX(${agX*2}px)
                               translateY(${agZ*2}px) 
