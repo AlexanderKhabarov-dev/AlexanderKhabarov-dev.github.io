167be0f8403c5db515d8c233964e677a8d0974c6
AlexanderKhabarov-dev <sanek737373@inbox.ru>
2020-04-30 09:35:59 +0400

    del pack

--- test-2/js/preloader.js
+++
@@ -1,9 +0,0 @@
-window.onload = () =>{
-    let preloader = document.querySelector('.preloader');
-
-    TweenMax.to(preloader, 1, {
-        display: 'none', 
-        opacity: 0,
-        //delay: 3,
-    })   
-}
