7fb518531a73a6933295f18118fb7dc1b188f2fb
AlexanderKhabarov-dev <sanek737373@inbox.ru>
2020-06-30 18:07:44 +0400

    digital

---
+++ digital/app/js/app.js
@@ -0,0 +1,14 @@
+svg4everybody({
+  validate: function (src, svg, use) {
+    return true;
+  }
+});
+
+
+function test(a,b){
+  console.log(`${a} + ${b} = ${a+b}` )
+}
+
+test(1,3);
+
+console.log('hello');
--- particles.js/js/script.js
+++ particles.js/js/script.js
@@ -1,10 +1,20 @@
 window.onload = () =>{
+
+    let form = document.querySelector('.form');
     let title1 = document.querySelector('#title_1');
     let title2 = document.querySelector('#title_2');
+    
 
+    // появление/исчезание заголовка через css
     title1.classList.add('title_active')
 
+    // появление второго заголовка
     setTimeout(() => {
-        title2.style.opacity = 1;
+        title2.classList.add('title_active')
     },2000)
+
+    setTimeout(() =>{
+        form.style.opacity = 1;
+    },4000)
+
 }
